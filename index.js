e8fdff22050f9db2aa87ae67a7daf77edfed69cc
An Nuyen <annuyen@gmail.com>
2018-01-31 18:06:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,46 @@
+let driverId = 0
+
+class Driver {
+	constructor(name){
+		this.id = ++driverId
+		this.name = name 
+
+		// insert in the user to the store
+		store.users.push(this)
+	}
+	items(){
+		return store.items.filter(item => {
+			return item.userId === this.id 
+		})
+	}
+}
+
+// below contains methoc that returns the user associated with the item
+
+class Passenger {
+	constructor(name, price, user){
+		this.id = ++itemId
+		this.name = name 
+		this.price = price 
+		if(user){
+			this.userId = user.id 
+		}
+
+		store.items.push(this) 
+	}
+
+	setUser(user){
+		this.userId = user.id 
+	}
+	user(){
+		return store.users.find(function(user){
+			return user.id === this.userId 
+		})
+	}
+}
+
+class Trip {
+  constructor(){
+    
+  }
+}
